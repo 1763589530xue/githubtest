0e58bec8e78b4f1c3b40c4e3f37180dcd1918521
xjp <xjp@163.com>
2020-08-12 19:34:05 +0800

    first git

---
+++ src/main/java/com/atguigu/github/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.github;
+
+/**
+ * @author xjp
+ * @create 2020-08-12 19:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
